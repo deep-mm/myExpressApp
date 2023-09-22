3acaca33ac2457e5c7d088d93b3e58c8f5800f7f
DEEP MEHTA <deep.mm@somaiya.edu>
2023-09-22 22:32:10 +0000

    Update app to allow access to db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
+const mysql = require('mysql2');
 var router = express.Router();
+require('dotenv').config({ path: './TEST.env' })
+
+// MySQL Database Connection
+const db = mysql.createConnection({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 519 - DMMEHTA2' });
-});
+  // Generate a random (x, y) point between (0, 0) and (100, 100)
+  const randomX = Math.random() * 100;
+  const randomY = Math.random() * 100;
 
+  // Find the closest point in the database
+  db.query(
+    `SELECT x, y, SQRT(POW(x - ?, 2) + POW(y - ?, 2)) AS distance
+    FROM random_points
+    ORDER BY distance
+    LIMIT 1`,
+    [randomX, randomY],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      if (results.length === 0) {
+        res.render('index', { title: 'CSC 519 - DMMEHTA2 - No distance'});
+      }
+
+      const closestPoint = results[0];
+      const distance = closestPoint.distance;
+
+      res.render('index', { title: 'CSC 519 - DMMEHTA2', x1: randomX, y1: randomY, x2: closestPoint.x, y2: closestPoint.y, distance: distance });
+    }
+  );
+
+  
+  });
 module.exports = router;
